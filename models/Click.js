ef356810baaf84c0b975521277f3f6912854dd95
Rahul Kumar <rah0987654321@gmail.com>
2025-11-11 22:36:01 +0530

    first commit

--- models/Click.js
+++ models/Click.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const clickSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
     affiliateCode: { type: String, required: true, index: true },
     ip: String,
     userAgent: String,
